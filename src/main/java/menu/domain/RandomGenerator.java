d85ff9025780369ffc02d60692960ce9c614c619
sinequanonn <dleck288@gmail.com>
2026-01-05 00:54:30 +0900

    feat: 무작위 카테고리 추출 기능 구현

---
+++ src/main/java/menu/domain/RandomGenerator.java
@@ -0,0 +1,14 @@
+package menu.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomGenerator {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 4;
+
+    public String generateCategory(List<String> categories) {
+        return categories.get(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+    }
+}
