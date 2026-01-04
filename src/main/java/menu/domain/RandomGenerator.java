05ba2b186ce1b9639f5a9dacacb0297f65719ebf
sinequanonn <dleck288@gmail.com>
2026-01-05 00:58:49 +0900

    feat: 무작위 메뉴 추출 기능 구현

--- src/main/java/menu/domain/RandomGenerator.java
+++ src/main/java/menu/domain/RandomGenerator.java
@@ -7,8 +7,13 @@ import java.util.List;
 public class RandomGenerator {
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 4;
+    public static final int MENU_ORDER = 0;
 
     public String generateCategory(List<String> categories) {
         return categories.get(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+    }
+
+    public String generateMenu(List<String> menus) {
+        return Randoms.shuffle(menus).get(MENU_ORDER);
     }
 }
